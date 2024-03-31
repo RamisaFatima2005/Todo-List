3a64ec636e7ced62571dc8afaca79b68fd541903
Ramisa <ramisafatima67@gmail.com>
2024-03-31 06:40:43 +0500

    This is CLI based Todo List

---
+++ main.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+console.log("\n \t Welcome to Todo-List Application\n")
+let condition = true;
+
+while(condition)
+{
+let addTask = await inquirer.prompt(
+    [
+        {
+            name:"todo",
+            type:"input",
+            message:"What you want to add in your Todo List?"
+        }
+    ]
+ );
+todos.push(addTask.todo)
+ console.log(`${addTask.todo}  added in your todo list successfully`)
+
+    let addMoreTask = await inquirer.prompt(
+        [
+        {
+            name:"addMore",
+            type:"confirm",
+            message:"Do you want to add more things in your Todo List?",
+            default:"false",
+        },
+    ]
+);
+condition = addMoreTask.addMore
+}; 
+console.log("Todo List: ",todos)
